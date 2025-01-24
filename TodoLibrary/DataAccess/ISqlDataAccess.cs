1c894d318b076a272381f4a412613d3ca60f7f01
UzairKhan-27 <m.uzairkhan1000@gmail.com>
2025-01-24 20:28:55 +0500

    add data access

---
+++ TodoLibrary/DataAccess/ISqlDataAccess.cs
@@ -0,0 +1,9 @@
+﻿
+namespace TodoLibrary.DataAccess
+{
+    public interface ISqlDataAccess
+    {
+        Task<List<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionStringName);
+        Task SavedData<T>(string storedProcedure, T parameters, string connectionStringName);
+    }
+}
